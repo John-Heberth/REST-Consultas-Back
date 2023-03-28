3b99d71f8c6647dd4b5dcb33dcb6e69e3a2d5676
John Heberth <johnpereira.df@gmail.com>
2023-03-28 16:56:46 -0300

    Novo Projeto

---
+++ src/main/java/com/heberth/rest/RestConsultaBackApplication.java
@@ -0,0 +1,13 @@
+package com.heberth.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConsultaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestConsultaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heberth/rest/RestConsultaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heberth.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestConsultaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
